ad10822a80de08c3544394e27b60cfc526d1cc26
prima keza <primakez@gmail.com>
2025-09-08 17:23:35 +0200

    fixed error

--- src/component/Carticon.tsx
+++ src/component/Carticon.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link"
-import Image from "next/image";
+
 import { FaCartArrowDown } from "react-icons/fa";
 import { useCart } from "@/context/CartContext";
 
--- src/component/CountDown.tsx
+++ src/component/CountDown.tsx
@@ -1,33 +1,25 @@
-
-
-
-// WITHOUT A LIBRARY
 "use client"
 import React, { useState, useEffect } from "react";
 
 const CountDown = () => {
-  
-  let difference = + new Date(`10/19/2026`) - + new Date();
-  const [delay, setDelay] = useState(difference);
+  const targetDate = new Date("10/19/2026").getTime();
+  const [difference, setDifference] = useState(
+    targetDate - new Date().getTime()
+  );
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDifference(targetDate - new Date().getTime());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [targetDate]);
 
   const d = Math.floor(difference / (1000 * 60 * 60 * 24));
   const h = Math.floor((difference / (1000 * 60 * 60)) % 24);
   const m = Math.floor((difference / 1000 / 60) % 60);
   const s = Math.floor((difference / 1000) % 60);
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setDelay(delay - 1);
-    }, 1000);
-
-    if (delay === 0) {
-      clearInterval(timer);
-    }
-
-    return () => {
-      clearInterval(timer);
-    };
-  });
   return (
     <span className="font-bold text-5xl text-yellow-400">
       {d}:{h}:{m}:{s}
@@ -36,3 +28,4 @@ const CountDown = () => {
 };
 
 export default CountDown;
+n
